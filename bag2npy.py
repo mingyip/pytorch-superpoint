f8daf961eac466dad4179a4be696ea4f222d6787
mingyip <fypcheung@gmail.com>
2020-02-17 22:01:29 +0100

    generate event data with ros lib

--- bag2npy.py
+++ bag2npy.py
@@ -30,54 +30,95 @@ from pathlib import Path
 #     types.append(bag.get_type_and_topic_info()[1].values()[i][0])
 # print(types)
 
-dataset = ["draw_lines", "draw_polygon", "draw_multiple_polygons", "draw_ellipses", "draw_star"]
+dataset = ["draw_lines", "draw_polygon", "draw_multiple_polygons", "draw_ellipses", "draw_star", "draw_checkerboard", "draw_stripes", "draw_cube"]
 dtype = ["training", "test", "validation"]
-dnum = [1000, 50, 20]
+dnum = [2000, 250, 100]
+
+
+
+# bag = rosbag.Bag("datasets/draw_lines/images/training/0135/0135.bag")
+
+# for i, (topic, msg, time) in enumerate(bag.read_messages(topics=['/cam0/events'])):
+#     print(i)
+
+# raise
+
 
 for data in dataset:
     for data_type, num in zip(dtype, dnum):
-
-        
-        ev_folder = Path("pytorch-superpoint/datasets/", data, "events", data_type)
-        cm_folder = Path("pytorch-superpoint/datasets/", data, "combined", data_type)
-        ev_folder.mkdir(parents=True, exist_ok=True)
-        cm_folder.mkdir(parents=True, exist_ok=True)
-
         for idx in range(num):
 
-            bag_folder = Path("pytorch-superpoint/datasets/", data, "images", data_type, "{:04d}".format(idx), "{:04d}.bag".format(idx))
-            ev_idx_folder = Path(ev_folder, "{:04d}".format(idx))
-            cm_idx_folder = Path(cm_folder, "{:04d}".format(idx))
-            ev_idx_folder.mkdir(parents=True, exist_ok=True)
-            cm_idx_folder.mkdir(parents=True, exist_ok=True)
+
+            print(data, data_type, idx)
+
+
+            bag_folder = Path("datasets/", data, "images", data_type, "{:04d}".format(idx), "{:04d}.bag".format(idx))
+            ev_folder = Path("datasets/", data, "events", data_type, "{:04d}".format(idx))
+            cm_folder = Path("datasets/", data, "combined", data_type, "{:04d}".format(idx))
+            gray_folder = Path("datasets/", data, "gray", data_type, "{:04d}".format(idx))
+            linear_decay_folder = Path("datasets/", data, "linear", data_type, "{:04d}".format(idx))
+            exponential_decay_folder = Path("datasets/", data, "expo", data_type, "{:04d}".format(idx))
+
+            ev_folder.mkdir(parents=True, exist_ok=True)
+            cm_folder.mkdir(parents=True, exist_ok=True)
+            gray_folder.mkdir(parents=True, exist_ok=True)
+            linear_decay_folder.mkdir(parents=True, exist_ok=True)
+            exponential_decay_folder.mkdir(parents=True, exist_ok=True)
 
             bag = rosbag.Bag(str(bag_folder))
 
+
+            start_nsec = 0
             for i, (topic, msg, time) in enumerate(bag.read_messages(topics=['/cam0/events'])):
 
-                print(data, " ", data_type, " ", idx, " ", i)
-                img_path = Path("pytorch-superpoint/datasets/", data, "images", data_type, "{:04d}".format(idx), "{}.png".format(i))
-                pts_path = Path("pytorch-superpoint/datasets/", data, "points", data_type, "{:04d}".format(idx), "{}.npy".format(i))
+                # print(data, " ", data_type, " ", idx, " ", i)
+                img_path = Path("datasets/", data, "images", data_type, "{:04d}".format(idx), "{:02d}.png".format(i))
+                pts_path = Path("datasets/", data, "points", data_type, "{:04d}".format(idx), "{:02d}.npy".format(i))
 
-                print(img_path)
+                # print(img_path)
                 img = cv.imread(str(img_path))
                 bg = np.zeros_like(img)
+                linear = np.zeros_like(img)
+                exponential = np.zeros_like(img)
                 pts = np.load(str(pts_path))
-        
+     
 
-                for e in msg.events:
+                total_time = time.to_nsec() - start_nsec
+                decay_constant = -0.000001
+
+
+                for j, e in enumerate(msg.events):
+                    t = e.ts.to_nsec() - start_nsec
+                    linear_decay = (t / total_time) * 255
+                    exponential_decay = np.exp(decay_constant * t) * 255
+
                     if e.polarity:
                         img[e.y, e.x] = [0, 0, 255]
                         bg[e.y, e.x] = [0, 0, 255]
+                        linear[e.y, e.x] = [0, 0, linear_decay]
+                        exponential[e.y, e.x] = [0, 0, exponential_decay]
                     else:
                         img[e.y, e.x] = [255, 0, 0]
                         bg[e.y, e.x] = [255, 0, 0]
+                        linear[e.y, e.x] = [linear_decay * 255, 0, 0]
+                        exponential[e.y, e.x] = [exponential_decay * 255, 0, 0]
 
 
                 for pt in pts:
                     cv.circle(img, (int(pt[1]), int(pt[0])), 1, (0, 255, 0), -1)
 
-                cv.imwrite(str(Path(ev_idx_folder, "{}.png".format(i))), bg)
-                cv.imwrite(str(Path(cm_idx_folder, "{}.png".format(i))), img)
+                gray = cv.cvtColor(bg, cv.COLOR_BGR2GRAY)
+                linear = cv.cvtColor(linear, cv.COLOR_BGR2GRAY)
+                exponential = cv.cvtColor(exponential, cv.COLOR_BGR2GRAY)
+
+                cv.imwrite(str(Path(ev_folder, "{}.png".format(i))), bg)
+                cv.imwrite(str(Path(cm_folder, "{}.png".format(i))), img)
+                cv.imwrite(str(Path(gray_folder, "{}.png".format(i))), gray)
+                cv.imwrite(str(Path(linear_decay_folder, "{}.png".format(i))), linear)
+                cv.imwrite(str(Path(exponential_decay_folder, "{}.png".format(i))), exponential)
+
+                start_nsec = time.to_nsec()
+
+
 
 
--- img2bag.sh
+++ img2bag.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 
-# for i in "draw_lines" "draw_polygon" "draw_multiple_polygons" "draw_ellipses draw_star"
-for i in "draw_ellipses" "draw_star"
+for i in "draw_lines" "draw_polygon" "draw_multiple_polygons" "draw_ellipses" "draw_star" "draw_checkerboard" "draw_cube" "draw_stripes"
+#for i in "draw_checkerboard" "draw_cube"
+# for i in "draw_stripes"
 do
 	
 
   for j in "training" "test" "validation"
   do
-    
-   
-    for k in {0000..0999}
+
+    if [ "$j" == "training" ]; then
+      num=1999
+    fi
+    if [ "$j" == "test" ]; then
+      num=0249
+    fi
+    if [ "$j" == "validation" ]; then
+      num=0099
+    fi
+
+    for k in $(eval echo "{0000..$num}")
     do
+      echo $j $k
       python scripts/generate_stamps_file.py -i /media/pytorch-superpoint/datasets/$i/images/$j/$k -r 1200.0
 
       rosrun esim_ros esim_node --data_source=2 --path_to_output_bag=/media/pytorch-superpoint/datasets/$i/images/$j/$k/$k.bag --path_to_data_folder=/media/pytorch-superpoint/datasets/$i/images/$j/$k/ --ros_publisher_frame_rate=60 --exposure_time_ms=10.0 --use_log_image=1 --log_eps=0.1   --contrast_threshold_pos=0.15 --contrast_threshold_neg=0.15
