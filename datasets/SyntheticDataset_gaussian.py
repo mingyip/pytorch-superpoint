096f5a4a9dea3632ae250d90df952e9012fc5e8e
mingyip <fypcheung@gmail.com>
2020-02-24 03:50:40 +0100

    add event to the dataLoader

--- datasets/SyntheticDataset_gaussian.py
+++ datasets/SyntheticDataset_gaussian.py
@@ -18,6 +18,7 @@ import tarfile
 # import os
 import random
 import logging
+import time
 
 from utils.tools import dict_update
 
@@ -33,13 +34,18 @@ from settings import DATA_PATH
 from settings import SYN_TMPDIR
 
 # DATA_PATH = '.'
-import multiprocessing
+# import multiprocessing
 
 TMPDIR = SYN_TMPDIR  # './datasets/' # you can define your tmp dir
 
 
 def load_as_float(path):
-    return imread(path).astype(np.float32) / 255
+    if path.endswith(".png"):
+        return imread(path).astype(np.float32) / 255
+    elif path.endswith(".npy"):
+        return (np.load(path)).astype(np.float32) / 255
+    else:
+        raise UnknownDataFormat() 
 
 
 class SyntheticDataset_gaussian(data.Dataset):
@@ -93,13 +99,13 @@ class SyntheticDataset_gaussian(data.Dataset):
     else:
         drawing_primitives = [
             "draw_lines",
-            "draw_polygon",
-            "draw_multiple_polygons",
-            "draw_ellipses",
-            "draw_star",
-            "draw_checkerboard",
-            "draw_stripes",
-            "draw_cube",
+            # "draw_polygon",
+            # "draw_multiple_polygons",
+            # "draw_ellipses",
+            # "draw_star",
+            # "draw_checkerboard",
+            # "draw_stripes",
+            # "draw_cube",
             # "gaussian_noise",
         ]
     print(drawing_primitives)
@@ -142,7 +148,6 @@ class SyntheticDataset_gaussian(data.Dataset):
                                                                 **primitive_config
                                                             )
 
-                # old_pts = None
 
                 for j, (pts, img) in enumerate(zip(pts_list, img_list)):
                     if j > frames:
@@ -181,8 +186,6 @@ class SyntheticDataset_gaussian(data.Dataset):
                     cv2.imwrite(str(Path(im_folder, "{:02d}.png".format(j))), img)
                     cv2.imwrite(str(Path(chk_folder, "{:02d}.png".format(j))), check_img)
                     np.save(Path(pts_folder, "{:02d}.npy".format(j)), pts)
-
-                    # old_pts = pts
 
 
         # Pack into a tar file
@@ -247,12 +250,12 @@ class SyntheticDataset_gaussian(data.Dataset):
         self.cell_size = 8
         self.getPts = getPts
 
-        self.gaussian_label = False
-        if self.config["gaussian_label"]["enable"]:
-            # self.params_transform = {'crop_size_y': 120, 'crop_size_x': 160, 'stride': 1, 'sigma': self.config['gaussian_label']['sigma']}
-            self.gaussian_label = True
+        self.gaussian_label = self.config["gaussian_label"]["enable"] == True
+        # if self.config["gaussian_label"]["enable"]:
+        #     # self.params_transform = {'crop_size_y': 120, 'crop_size_x': 160, 'stride': 1, 'sigma': self.config['gaussian_label']['sigma']}
+        #     self.gaussian_label = True
 
-        self.pool = multiprocessing.Pool(6)
+        # self.pool = multiprocessing.Pool(6)
 
         # Parse drawing primitives
         primitives = self.parse_primitives(
@@ -266,12 +269,12 @@ class SyntheticDataset_gaussian(data.Dataset):
         )
         basepath.mkdir(parents=True, exist_ok=True)
 
-        splits = {s: {"images": [], "points": []} for s in [self.action]}
+        splits = {s: {"images": [], "events": [], "points": []} for s in [self.action]}
 
-        for primitive in primitives:
-            tar_path = Path(basepath, "{}.tar.gz".format(primitive))
-            if not tar_path.exists():
-                self.dump_primitive_data(primitive, tar_path, self.config)
+        # for primitive in primitives:
+        #     tar_path = Path(basepath, "{}.tar.gz".format(primitive))
+        #     if not tar_path.exists():
+        #         self.dump_primitive_data(primitive, tar_path, self.config)
 
             # # Untar locally
             # logging.info("Extracting archive for primitive {}.".format(primitive))
@@ -288,29 +291,31 @@ class SyntheticDataset_gaussian(data.Dataset):
             truncate = self.config["truncate"].get(primitive, 1)
             path = Path(temp_dir, primitive)
             for s in splits:
-                e = [str(p) for p in Path(path, "images", s).iterdir()]
-                f = [p.replace("images", "points") for p in e]
-                f = [p.replace(".png", ".npy") for p in f]
-                splits[s]["images"].extend(e[: int(truncate * len(e))])
+                i = [str(p) for p in Path(path, "images", s).iterdir()]
+                e = [p.replace("images", "ev_expo").replace(".png", ".npy") for p in i]
+                f = [p.replace("images", "points").replace(".png", ".npy") for p in i]
+                splits[s]["images"].extend(i[: int(truncate * len(i))])
+                splits[s]["events"].extend(e[: int(truncate * len(e))])
                 splits[s]["points"].extend(f[: int(truncate * len(f))])
 
         # Shuffle
         for s in splits:
-            perm = np.random.RandomState(0).permutation(len(splits[s]["images"]))
-            for obj in ["images", "points"]:
+            perm = np.random.RandomState(0).permutation(len(splits[s]["events"]))
+            for obj in ["images", "events", "points"]:
                 splits[s][obj] = np.array(splits[s][obj])[perm].tolist()
 
         self.crawl_folders(splits)
 
+
     def crawl_folders(self, splits):
         sequence_set = []
-        for (img, pnts) in zip(
-            splits[self.action]["images"], splits[self.action]["points"]
+        for (imgs, evts, pnts) in zip(
+            splits[self.action]["images"], splits[self.action]["events"], splits[self.action]["points"]
         ):
-            sample = {"image": img, "points": pnts}
+            sample = {"images": imgs, "events": evts, "points": pnts}
             sequence_set.append(sample)
         self.samples = sequence_set
-
+ 
 
     # def putGaussianMaps_par(self, center):
     #     crop_size_y = self.params_transform['crop_size_y']
@@ -381,6 +386,9 @@ class SyntheticDataset_gaussian(data.Dataset):
             img = cusAug(img, **self.config["augmentation"])
             return img
 
+        # def imgHomographic(img):
+        #     pass
+
         def get_labels(pnts, H, W):
             labels = torch.zeros(H, W)
             # print('--2', pnts, pnts.size())
@@ -388,6 +396,7 @@ class SyntheticDataset_gaussian(data.Dataset):
             pnts_int = torch.min(
                 pnts.round().long(), torch.tensor([[W - 1, H - 1]]).long()
             )
+
             # print('--3', pnts_int, pnts_int.size())
             labels[pnts_int[:, 1], pnts_int[:, 0]] = 1
             return labels
@@ -403,140 +412,132 @@ class SyntheticDataset_gaussian(data.Dataset):
             labels_res = labels_res.transpose(1, 2).transpose(0, 1)
             return labels_res
 
+        def output_test_img(img, pnts=[], idx=0):
+            if len(img.shape) == 2:
+                img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+            for pt in pnts:
+                cv2.circle(img, (pt[0], pt[1]), 2, (0, 255, 0), -1)
+            cv2.imwrite(str(Path("temp", "{:02d}.png".format(idx))), img * 255)
+
+
         from datasets.data_tools import np_to_tensor
         from utils.utils import filter_points
         from utils.var_dim import squeezeToNumpy
 
+
         sample = self.samples[index]
-        img = load_as_float(sample["image"])
-        H, W = img.shape[0], img.shape[1]
-        self.H = H
-        self.W = W
+        img = load_as_float(sample["images"])
+        evt = load_as_float(sample["events"])
+        H, W, C = evt.shape
+
         pnts = np.load(sample["points"])  # (y, x)
         pnts = torch.tensor(pnts).float()
         pnts = torch.stack((pnts[:, 1], pnts[:, 0]), dim=1)  # (x, y)
         pnts = filter_points(pnts, torch.tensor([W, H]))
         sample = {}
 
-        # print('pnts: ', pnts[:5])
-        # print('--1', pnts)
-        labels_2D = get_labels(pnts, H, W)
-        sample.update({"labels_2D": labels_2D.unsqueeze(0)})
+        # labels_2D = get_labels(pnts, H, W)
+        # sample.update({"labels_2D": labels_2D.unsqueeze(0)})
 
+        if self.action == "training":
+            do_Photometrix = self.config["augmentation"]["photometric"]["enable_train"]
+            do_Homographic = self.config["augmentation"]["homographic"]["enable_train"]
+        elif self.action == "validation":
+            do_Photometrix = self.config["augmentation"]["photometric"]["enable_val"]
+            do_Homographic = self.config["augmentation"]["homographic"]["enable_val"]
+        else:
+            do_Photometrix = False
+            do_Homographic = False
+
+
+        # TODO: remove the sanity check part
+        # output_test_img(img, pnts, 0)
+        
         # assert Hc == round(Hc) and Wc == round(Wc), "Input image size not fit in the block size"
-        if (
-            self.config["augmentation"]["photometric"]["enable_train"]
-            and self.action == "training"
-        ) or (
-            self.config["augmentation"]["photometric"]["enable_val"]
-            and self.action == "validation"
-        ):
-            # print('>>> Photometric aug enabled for %s.'%self.action)
-            # augmentation = self.ImgAugTransform(**self.config["augmentation"])
+        if do_Photometrix:
             img = imgPhotometric(img)
-        else:
-            # print('>>> Photometric aug disabled for %s.'%self.action)
-            pass
+        # output_test_img(img, pnts, 1)
 
-        if not (
-            (
-                self.config["augmentation"]["homographic"]["enable_train"]
-                and self.action == "training"
-            )
-            or (
-                self.config["augmentation"]["homographic"]["enable_val"]
-                and self.action == "validation"
-            )
-        ):
-            # print('<<< Homograpy aug disabled for %s.'%self.action)
-            img = img[:, :, np.newaxis]
-            # labels = labels.view(-1,H,W)
-            if self.transform is not None:
-                img = self.transform(img)
-            sample["image"] = img
-            # sample = {'image': img, 'labels_2D': labels}
-            valid_mask = self.compute_valid_mask(
-                torch.tensor([H, W]), inv_homography=torch.eye(3)
-            )
-            sample.update({"valid_mask": valid_mask})
-            labels_res = get_label_res(H, W, pnts)
-            pnts_post = pnts
-            # pnts_for_gaussian = pnts
-        else:
-            # print('>>> Homograpy aug enabled for %s.'%self.action)
-            # img_warp = img
+        if do_Homographic:
+            print('<<< Homograpy aug enabled for %s.'%self.action)
             from utils.utils import homography_scaling_torch as homography_scaling
-            from numpy.linalg import inv
+
+            img = torch.from_numpy(img)
+            erosion_radius=self.config["augmentation"]["homographic"]["valid_border_margin"]
 
             homography = self.sample_homography(
                 np.array([2, 2]),
                 shift=-1,
                 **self.config["augmentation"]["homographic"]["params"],
             )
-
-            ##### use inverse from the sample homography
-            homography = inv(homography)
-            ######
-
+            homography = np.linalg.inv(homography) # use inverse from the sample homography
             homography = torch.tensor(homography).float()
             inv_homography = homography.inverse()
-            img = torch.from_numpy(img)
-            warped_img = self.inv_warp_image(
-                img.squeeze(), inv_homography, mode="bilinear"
-            )
+
+            warped_pnts = self.warp_points(pnts, homography_scaling(homography, H, W))
+            warped_pnts = filter_points(warped_pnts, torch.tensor([W, H]))
+
+            warped_img = self.inv_warp_image(img.squeeze(), inv_homography, mode="bilinear")
             warped_img = warped_img.squeeze().numpy()
             warped_img = warped_img[:, :, np.newaxis]
 
-            # labels = torch.from_numpy(labels)
-            # warped_labels = self.inv_warp_image(labels.squeeze(), inv_homography, mode='nearest').unsqueeze(0)
-            warped_pnts = self.warp_points(pnts, homography_scaling(homography, H, W))
-            warped_pnts = filter_points(warped_pnts, torch.tensor([W, H]))
-            # pnts = warped_pnts[:, [1, 0]]
-            # pnts_for_gaussian = warped_pnts
-            # warped_labels = torch.zeros(H, W)
-            # warped_labels[warped_pnts[:, 1], warped_pnts[:, 0]] = 1
-            # warped_labels = warped_labels.view(-1, H, W)
+            # output_test_img(warped_img.squeeze(), warped_pnts.numpy().squeeze(), 2)
+            warped_img = self.transform(warped_img) if self.transform else warped_img
+            # output_test_img(warped_img.numpy().squeeze(), warped_pnts.numpy().squeeze(), 3)
+            
+        else:
+            print('<<< Homograpy aug disabled for %s.'%self.action)
+            img = img[:, :, np.newaxis]
+            warped_img = self.transform(img) if self.transform else img
+            warped_pnts = pnts
+            erosion_radius = 0
 
-            if self.transform is not None:
-                warped_img = self.transform(warped_img)
-            # sample = {'image': warped_img, 'labels_2D': warped_labels}
-            sample["image"] = warped_img
 
-            valid_mask = self.compute_valid_mask(
-                torch.tensor([H, W]),
-                inv_homography=inv_homography,
-                erosion_radius=self.config["augmentation"]["homographic"][
-                    "valid_border_margin"
-                ],
-            )  # can set to other value
-            sample.update({"valid_mask": valid_mask})
+        valid_mask = self.compute_valid_mask(
+            torch.tensor([H, W]),
+            inv_homography=inv_homography,
+            erosion_radius=erosion_radius,
+        )
 
-            labels_2D = get_labels(warped_pnts, H, W)
-            sample.update({"labels_2D": labels_2D.unsqueeze(0)})
 
-            labels_res = get_label_res(H, W, warped_pnts)
-            pnts_post = warped_pnts
+        # TODO: remove the sanity check part
+        # output_test_img(warped_img.numpy().squeeze(), warped_pnts.numpy().squeeze(), 4)
+        # output_test_img(valid_mask.numpy().squeeze(), warped_pnts.numpy().squeeze(), 5)
+        
+        labels_2D = get_labels(warped_pnts, H, W)
+        labels_res = get_label_res(H, W, warped_pnts)
+        sample["image"] = warped_img
+        sample.update({"valid_mask": valid_mask})
+        sample.update({"labels_2D": labels_2D.unsqueeze(0)})
+        sample.update({"labels_res": labels_res})
 
+        # TODO: remove the sanity check part
+        temp = cv2.cvtColor(warped_img.numpy().squeeze(), cv2.COLOR_GRAY2RGB)
+        temp[:, :, 1] = labels_2D.numpy().squeeze() * 255
+        # output_test_img(temp, [], 6)
+
+        
         if self.gaussian_label:
-            # warped_labels_gaussian = get_labels_gaussian(pnts)
             from datasets.data_tools import get_labels_bi
 
-            labels_2D_bi = get_labels_bi(pnts_post, H, W)
+            labels_2D_bi = get_labels_bi(warped_pnts, H, W)
 
             labels_gaussian = self.gaussian_blur(squeezeToNumpy(labels_2D_bi))
             labels_gaussian = np_to_tensor(labels_gaussian, H, W)
             sample["labels_2D_gaussian"] = labels_gaussian
 
-            # add residua
+            # output_test_img(labels_gaussian.numpy().squeeze(), [], 7)
 
-        sample.update({"labels_res": labels_res})
 
         ### code for warped image
-        if self.config["warped_pair"]["enable"]:
+        # if self.config["warped_pair"]["enable"]:
+        if True:
             from datasets.data_tools import warpLabels
 
             homography = self.sample_homography(
-                np.array([2, 2]), shift=-1, **self.config["warped_pair"]["params"]
+                np.array([2, 2]), 
+                shift=-1, 
+                **self.config["warped_pair"]["params"]
             )
 
             ##### use inverse from the sample homography
@@ -551,17 +552,13 @@ class SyntheticDataset_gaussian(data.Dataset):
 
             # warp original image
             warped_img = img.type(torch.FloatTensor)
-            warped_img = self.inv_warp_image(
-                warped_img.squeeze(), inv_homography, mode="bilinear"
-            ).unsqueeze(0)
-            if (self.enable_photo_train == True and self.action == "train") or (
-                self.enable_photo_val and self.action == "val"
-            ):
-                warped_img = imgPhotometric(
-                    warped_img.numpy().squeeze()
-                )  # numpy array (H, W, 1)
+            warped_img = self.inv_warp_image(warped_img.squeeze(), inv_homography, mode="bilinear").unsqueeze(0)
+
+            if (self.enable_photo_train and self.action == "train") or \
+               (self.enable_photo_val and self.action == "val"):
+                warped_img = imgPhotometric(warped_img.numpy().squeeze())  # numpy array (H, W, 1)
                 warped_img = torch.tensor(warped_img, dtype=torch.float32)
-                pass
+
             warped_img = warped_img.view(-1, H, W)
 
             # warped_labels = warpLabels(pnts, H, W, homography)
@@ -569,11 +566,8 @@ class SyntheticDataset_gaussian(data.Dataset):
             warped_labels = warped_set["labels"]
             warped_res = warped_set["res"]
             warped_res = warped_res.transpose(1, 2).transpose(0, 1)
-            # print("warped_res: ", warped_res.shape)
+
             if self.gaussian_label:
-                # print("do gaussian labels!")
-                # warped_labels_gaussian = get_labels_gaussian(warped_set['warped_pnts'].numpy())
-                # warped_labels_bi = self.inv_warp_image(labels_2D.squeeze(), inv_homography, mode='nearest').unsqueeze(0) # bilinear, nearest
                 warped_labels_bi = warped_set["labels_bi"]
                 warped_labels_gaussian = self.gaussian_blur(
                     squeezeToNumpy(warped_labels_bi)
@@ -601,34 +595,22 @@ class SyntheticDataset_gaussian(data.Dataset):
                 {"homographies": homography, "inv_homographies": inv_homography}
             )
 
-        # labels = self.labels2Dto3D(self.cell_size, labels)
-        # labels = torch.from_numpy(labels[np.newaxis,:,:])
-        # input.update({'labels': labels})
 
-        ### code for warped image
+# Image
+# Valid_Mask
+# Labels_2D
+# Labels_Res
+# Warped_Img
+# Warped_Labels
+# Warped_Res
+# Warped_Valid_Mask
+# Homographies
+# Inv_Homographies
 
-        # if self.config['gaussian_label']['enable']:
-        #     heatmaps = np.zeros((H, W))
-        #     # for center in pnts_int.numpy():
-        #     for center in pnts[:, [1, 0]].numpy():
-        #         # print("put points: ", center)
-        #         heatmaps = self.putGaussianMaps(center, heatmaps)
-        #     # import matplotlib.pyplot as plt
-        #     # plt.figure(figsize=(5, 10))
-        #     # plt.subplot(211)
-        #     # plt.imshow(heatmaps)
-        #     # plt.colorbar()
-        #     # plt.subplot(212)
-        #     # plt.imshow(np.squeeze(warped_labels.numpy()))
-        #     # plt.show()
-        #     # import time
-        #     # time.sleep(500)
-        #     # results = self.pool.map(self.putGaussianMaps_par, warped_pnts.numpy())
-
-        #     warped_labels_gaussian = torch.from_numpy(heatmaps).view(-1, H, W)
-        #     warped_labels_gaussian[warped_labels_gaussian>1.] = 1.
-
-        #     sample['labels_2D_gaussian'] = warped_labels_gaussian
+        print(sample)
+        for x in sample:
+            print(x.strip().title())
+        raise
 
         if self.getPts:
             sample.update({"pts": pnts})
